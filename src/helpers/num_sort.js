c3118d555c18e19b7c230aede0d1a3ccc4416b18
Vladislav Gorovoy <vldislav.operating@gmail.com>
2019-01-21 14:01:13 +0200

    [+] median

--- src/helpers/num_sort.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-function num_sort(array) {
-    return array.sort(function (a, b) { return a - b; });
-}
-exports.num_sort = num_sort;
--- src/standart_deviation/standart_deviation.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var variance_1 = require("../variance/variance");
-function standart_deviation(array) {
-    return Math.sqrt(variance_1["default"](array));
-}
-exports["default"] = standart_deviation;
