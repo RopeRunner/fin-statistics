521baf692e78ad0b594afa3c657c971436fcf837
Vladislav Gorovoy <vldislav.operating@gmail.com>
2019-01-22 13:05:46 +0200

    [+] linear weighted mobing avarage added

--- src/fin_instruments/index.ts
+++ src/fin_instruments/index.ts
@@ -1,1 +1,4 @@
 export { default as sma } from "./moving_avarages/sma/sma";
+export {
+  default as linear_weighted_moving_avarage
+} from "./moving_avarages/lwma/lwma";
---
+++ src/fin_instruments/moving_avarages/lwma/lwma.ts
@@ -0,0 +1,41 @@
+/**
+ * Function takes as a parameter list of data (numerical data)
+ * and window size
+ * and returns list of weighted moving avarages values for this period
+ * @param data_set {Array<number>} - numerical data set
+ * @param window_size {number} - numerical size of window
+ */
+function linear_weighted_moving_avarage(
+  data_set: Array<number>,
+  window_size: number
+): Array<number> | Error {
+  if (data_set.length <= window_size) {
+    return new Error("Lenght of data set need to be longer than window_size");
+  } else if (window_size <= 0) {
+    return new Error("Window size can not be less or equal to zero");
+  }
+  let result: Array<number> = [],
+    last_element: number = data_set.length - window_size + 1,
+    sub_sum: number = 0,
+    divider: number = window_size * (window_size - 1),
+    start: number = 0,
+    end: number = window_size - 1;
+
+  for (let i = 0; i < last_element; i++) {
+    data_set.forEach((el: number, idx: number) => {
+      if (idx < start || idx > end) {
+        return;
+      } else {
+        sub_sum += el * (idx + 1);
+      }
+    });
+    result.push((sub_sum * 2) / divider);
+    sub_sum = 0;
+    start++;
+    end++;
+  }
+
+  return result;
+}
+
+export default linear_weighted_moving_avarage;
