5c8f1f0c1ac2cc908d7568ed80df867d8c597a84
RopeRunner <glitchwave13@gmail.com>
2019-01-23 21:01:26 +0200

    [+, ^] added normal distribution, fixed variance

--- src/fundamental_statistics/variance/variance.ts
+++ src/fundamental_statistics/variance/variance.ts
@@ -9,13 +9,13 @@ function variance(data_set: Array<number>): number {
   if (data_set.length === 0) throw new Error("Data set can not be empty");
 
   const mean_val = mean(data_set);
-  let tmp_sqr_val: number = 0;
+  let summator: number = 0;
 
   for (let i = 0; i < data_set.length; i++) {
-    tmp_sqr_val += Math.pow(data_set[i], 2);
+    summator += Math.pow(data_set[i] - mean_val, 2);
   }
 
-  return tmp_sqr_val / data_set.length - Math.pow(mean_val, 2);
+  return summator / (data_set.length - 1);
 }
 
 export default variance;
